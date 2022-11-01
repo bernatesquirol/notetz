410df5539cf66a4445cafedfbd8c50f17bcc1e64
bernat <bernatesquirol@gmail.com>
2022-11-01 18:45:15 +0100

    sel

--- src/SelectSimple.tsx
+++ src/SelectSimple.tsx
@@ -1,3 +1,4 @@
-export default ({options,...props})=>(<select name="example" {...props}>
+const Selector =  ({options,...props})=>(<select name="example" {...props}>
   {options.map((option=><option value={option}>{option}</option>))}
 </select>)
+export default Selector
