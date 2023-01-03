bd898b6e4e5b34be98a5201bcf3bb6bf23cc7f3b
bernatesquirol <bernatesquirol@gmail.com>
2023-01-03 20:15:42 +0100

    update pad no dbl

--- src/Pad.tsx
+++ src/Pad.tsx
@@ -125,7 +125,7 @@ export function PlayingPad({width, height, notes}){
     }
   },[toggleVoice])
   const {setKey} = FlowSelectorContext.useFlowSelectorContext()
-  const onDblHitCell = useCallback((cell)=>setKey(cell.label),[setKey])
+  const onDblHitCell = useCallback((cell)=>{/*setKey(cell.label)*/},[setKey])
   const activeCells = useMemo(()=>{
     let active = Object.fromEntries(Object.entries(voices||{}).map(([k,v])=>(v?.map(voice=>([voice.id, k])))).flat().filter(d=>d))
     return active
